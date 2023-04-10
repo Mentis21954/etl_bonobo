367105f59cbb78e5e188a5188da78d4bc9cd86c3
Mentis21954 <it21954@hua.gr>
2023-04-10 21:41:55 +0300

    no idea

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# etl_bonobo
+# example ETL with pandas and bonobo
+
+- Data Visualization
+bonobo inspect --graph run.py | dot -Tpng -o etl.png
+- Run bonobo project
+bonobo run run.py
+
--- run.py
+++ run.py
@@ -1,24 +1,29 @@
 import bonobo
+import pandas as pd
+import time
+import json
+import requests
 
-class extract():
+LASTFM_API_KEY = '3f8f9f826bc4b0c8b529828839d38e4b'
+DISCOGS_API_KEY = 'hhNKFVCSbBWJATBYMyIxxjCJDSuDZMBGnCapdhOy'
 
-    def generate_data(self):
-        yield 'foo'
-        yield 'bar'
-        yield 'baz'
+def extract_info_from_artist(artists_names):
+    # extract for all artists' informations from last fm and store as a dict
+    for name in artists_names:
+        url = ('https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=') + str(name) + (
+            '&api_key=') + str(LASTFM_API_KEY) + ('&format=json')
+        artist_info = requests.get(url).json()
+        yield artist_info['artist']['bio']['content']
 
-def uppercase(x: str):
-    return x.upper()
-
-def output(x: str):
-    print(x)
-
+def print_data(content: str):
+    print(content)
 
 if __name__ == '__main__':
+    df = pd.read_csv('spotify_artist_data.csv')
+    artist_names = list(df['Artist Name'].unique())
 
     graph = bonobo.Graph(
-        extract.generate_data(),
-        uppercase,
-        output,
+        extract_info_from_artist(artist_names[2]),
+        print_data
     )
     bonobo.run(graph)
