890cd48ef49b769144c0cabfadf20934cf2d0bd8
Mentis21954 <it21954@hua.gr>
2023-04-22 22:21:50 +0300

    extract ok

--- run.py
+++ run.py
@@ -1,73 +1,104 @@
 import bonobo
 import pandas as pd
-import time
-import json
 import requests
+import time
 
 LASTFM_API_KEY = '3f8f9f826bc4b0c8b529828839d38e4b'
 DISCOGS_API_KEY = 'hhNKFVCSbBWJATBYMyIxxjCJDSuDZMBGnCapdhOy'
 
-
-def extract_info_from_artist(artist_names):
+def extract_info_from_artist(artist_names: list):
+    # extract for all artists' informations from last fm and store as a dict
     for name in artist_names:
-        # extract for all artists' informations from last fm and store as a dict
         url = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + str(name) + (
             '&api_key=') + str(LASTFM_API_KEY) + '&format=json'
         artist_info = requests.get(url).json()
         print('Search information for artist {} ...'.format(str(name)))
         yield {name: artist_info['artist']['bio']['content']}
 
+def extract_titles_from_artist(artist_names: list):
 
-def extract_titles_from_artist(artist_names):
     for name in artist_names:
         # get the artist id from artist name
-        url = 'https://api.discogs.com/database/search?q=' + str(name) + (
-            '&{?type=artist}&token=') + DISCOGS_API_KEY
+        url = ('https://api.discogs.com/database/search?q=') + name + ('&{?type=artist}&token=') + DISCOGS_API_KEY
         discogs_artist_info = requests.get(url).json()
         id = discogs_artist_info['results'][0]['id']
-
-        print('Search releases from discogs.com for artist {} ...'.format(str(name)))
-
         # with id get artist's releases
         url = ('https://api.discogs.com/artists/') + str(id) + ('/releases')
         releases = requests.get(url).json()
 
-        # store the tracks info in a list
-        title_info, colab_info, year_info, format_info, price_info = [], [], [], [], []
-        for index in range(len(releases['releases'])):
-            url = releases['releases'][index]['resource_url']
+        print('Found releases from discogs.com for artist ' + str(name) + ' with Discogs ID: ' + str(id))
+
+        yield {name: releases['releases']}
+
+def extract_info_for_titles(releases: dict):
+    # store the releases/tracks info in a list
+    releases_info = []
+
+    key = list(releases.keys())
+    artist = str(key[0])
+    for index in range(len(releases[artist])):
+        url = releases[artist][index]['resource_url']
+        source = requests.get(url).json()
+        # search if exists track's price
+        if 'lowest_price' in source.keys():
+            if 'formats' in source.keys():
+                releases_info.append({'Title': source['title'],
+                                      'Collaborations': releases[artist][index]['artist'],
+                                      'Year': source['year'],
+                                      'Format': source['formats'][0]['name'],
+                                      'Discogs Price': source['lowest_price']})
+            else:
+                releases_info.append({'Title': source['title'],
+                                      'Collaborations': releases[artist][index]['artist'],
+                                      'Year': source['year'],
+                                      'Format': None,
+                                      'Discogs Price': source['lowest_price']})
+        print("Found informations from discogs.com for {} {}'s titles".format(str((index + 1)), artist))
+        # sleep 3 secs to don't miss requests
+        time.sleep(3)
+
+    # return artist's tracks for transform stage
+    yield releases_info
+
+def extract_playcounts_from_titles_by_artist(releases: dict):
+    # initialize list for playcounts for each title
+    playcounts = []
+    # find playcounts from lastfm for each release title
+    key = list(releases.keys())
+    artist = str(key[0])
+    for index in range(len(releases[artist])):
+        title = releases[artist][index]['title']
+        print(title)
+        url = 'https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=' + LASTFM_API_KEY + '&artist=' + artist + '&track=' + title + '&format=json'
+
+        try:
             source = requests.get(url).json()
-            # search if exists track's price
-            if 'lowest_price' in source.keys():
+            if 'track' in source.keys():
+                playcounts.append({'Title': source['track']['name'],
+                                   'Lastfm Playcount': source['track']['playcount']})
+                print('Found playcount from last.fm for title {}'.format(title))
+            else:
+                print('Not found playcount from last.fm for title {}'.format(title))
+        except:
+            print('Not found playcount from last.fm for title {}'.format(title))
+            continue
 
-                title_info.append(source['title'])
-                colab_info.append(releases['releases'][index]['artist'])
-                year_info.append(source['year'])
-                price_info.append(source['lowest_price'])
-                if 'formats' in source.keys():
-                    format_info.append(source['formats'][0]['name'])
-                else:
-                    format_info.append(None)
-                print('Found ' + str((index + 1)) + ' titles!')
-
-            # sleep 3 secs to don't miss requests
-            time.sleep(1)
-
-        print('Find tracks from artist ' + str(name) + ' with Discogs ID: ' + str(id))
-        yield {'Title': title_info, 'Collaborations': colab_info, 'Year': year_info,
-               'Format': format_info, 'Discogs Price': price_info}
-
+    return playcounts
 
 def print_data(data):
     print(data)
 
 
 if __name__ == '__main__':
+    # find names from csv file
     df = pd.read_csv('spotify_artist_data.csv')
     artist_names = list(df['Artist Name'].unique())
-
+    artist_names = artist_names[:2
+                   ]
     graph = bonobo.Graph()
-    graph.add_chain(extract_info_from_artist(artist_names[:1]), print_data)
-    graph.add_chain(extract_titles_from_artist(artist_names[:1), print_data)
+    graph.add_chain(extract_info_from_artist(artist_names), print_data)
+    releases = extract_titles_from_artist(artist_names)
+    graph.add_chain(releases, extract_playcounts_from_titles_by_artist, print_data)
+    graph.add_chain(extract_info_for_titles, print_data, _input=releases)
 
     bonobo.run(graph)
